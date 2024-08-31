eda9f90891403a22dfaa21291dd92d045a8e588b
theassassin0128 <rafisarkar0128@gmail.com>
2024-08-31 06:29:08 +0600

    deleted files

--- src/schemas/errorlog.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require("mongoose");
-const errorLogSchema = new Schema({
-    guild: String,
-    channel: String,
-    enabled: String,
-});
-
-module.exports = model("errorlog", errorLogSchema);
